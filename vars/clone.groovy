01fd46ec02866b5197028592d9b4b207dd8aba9b
SimarSingh18 <91193457+SimarSingh18@users.noreply.github.com>
2024-12-03 20:14:42 +0530

    Update clone.groovy

--- vars/clone.groovy
+++ vars/clone.groovy
@@ -1,7 +1,6 @@
-def call(String url, String branch){
-
-  echo "this is cloning the code"
-  git url: ${url}, branch: ${branch}
-  echo "code cloned successfully"
-
+def call(String url, String branch) {
+  
+    echo "This is cloning the code"
+    git(url: url, branch: branch)
+    echo "Code cloned successfully"
 }
